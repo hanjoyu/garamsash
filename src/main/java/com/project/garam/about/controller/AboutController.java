6d4927866de5a7bc56a6d45d3abfdef84cbb2bb5
HANJO94\hanjo <hanjo94@gmail.com>
2023-04-16 19:33:48 +0900

    공통함수 분리

--- src/main/java/com/project/garam/about/controller/AboutController.java
+++ src/main/java/com/project/garam/about/controller/AboutController.java
@@ -1,9 +1,13 @@
 package com.project.garam.about.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.project.garam.common.CommonRoot;
 @Controller
@@ -11,8 +15,13 @@ import com.project.garam.common.CommonRoot;
 public class AboutController {
 	
 	@GetMapping("main")
-	public String aboutMain() {
+	public ModelAndView aboutMain(ModelAndView mv) {
+Map<String, String> mainMap = new HashMap<>();
 		
-		return CommonRoot.MAIN_ROOT + "about";
+		mainMap.put("mainYn", "N");
+		mv.addObject("mainParam", mainMap);
+		mv.setViewName(CommonRoot.MAIN_ROOT + "about");
+		
+		return mv;
 	}
 }
