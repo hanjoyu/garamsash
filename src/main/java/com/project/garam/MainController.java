2bf27ad1abea0ac561bb826dfe23c4489f7ea710
HANJO94\hanjo <hanjo94@gmail.com>
2023-04-16 18:41:56 +0900

    front 콩통함수 분리

--- src/main/java/com/project/garam/MainController.java
+++ src/main/java/com/project/garam/MainController.java
@@ -1,16 +1,28 @@
 package com.project.garam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.project.garam.common.CommonRoot;
+
+import ch.qos.logback.core.model.Model;
 
 @Controller
 public class MainController {
 	
 	@GetMapping(value= {"/", "/main"})
-	public String main() {
+	public ModelAndView main(ModelAndView mv) {
 
-		return CommonRoot.MAIN_ROOT + "main";
+		Map<String, String> mainMap = new HashMap<>();
+		
+		mainMap.put("mainYn", "Y");
+		mv.addObject("mainParam", mainMap);
+		mv.setViewName(CommonRoot.MAIN_ROOT + "main");
+		
+		return mv;
 	}
 }
