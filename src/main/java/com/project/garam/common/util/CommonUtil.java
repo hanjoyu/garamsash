be4e8ddd21dbcff515fee7d241fd5c2aa4f4471f
HANJO94\hanjo <hanjo94@gmail.com>
2023-05-01 18:53:04 +0900

    select 색 변경 타임리프 수정

--- src/main/java/com/project/garam/common/util/CommonUtil.java
+++ src/main/java/com/project/garam/common/util/CommonUtil.java
@@ -11,12 +11,21 @@ import com.project.garam.common.CommonConstant;
 @Component
 public class CommonUtil {
 
-	public ModelAndView mainYn(ModelAndView mv, Map<String, String> mainMap, String root) {
-		
+	public ModelAndView mainYn(ModelAndView mv, Map<String, String> mainMap, String viewName) {
+
+		String forPage = "";
+		if ("book".equals(viewName) || "team".equals(viewName) || "testimonial".equals(viewName)) {
+			forPage = "pages";
+			mainMap.put("viewName", forPage);
+			
+		} else {
+			mainMap.put("viewName", viewName);
+
+		}
 		mv.addObject("mainParam", mainMap);
-		mv.setViewName(CommonConstant.MAIN_ROOT + root);
-		
-		return mv; 
+		mv.setViewName(CommonConstant.MAIN_ROOT + viewName);
+
+		return mv;
 	}
-	
+
 }
